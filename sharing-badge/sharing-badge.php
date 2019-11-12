6d2b93833f36cabc2eb2dcb79cb9eed55e74a9fd
donwattz1459 <mcguinn.donald@gmail.com>
2019-11-12 12:06:10 -0500

    Updated code

--- sharing-badge/sharing-badge.php
+++ sharing-badge/sharing-badge.php
@@ -16,4 +16,22 @@
 	 
 		return $classes;
 	} );
+	
+	//GET CURRENT PAGE URL
+	//USE SHORTCODE [get_url} TO SHOW URL ON FRONT END
+	//===========================================
+	add_shortcode ('geturl', 'get_current_page_url');
+	function get_current_page_url() {
+		$pageURL = 'http';
+		if( isset($_SERVER["HTTPS"]) ) {
+			if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+		}
+		$pageURL .= "://";
+		if ($_SERVER["SERVER_PORT"] != "80") {
+			$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+		} else {
+			$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+		}
+		return $pageURL;
+	}
 ?>
