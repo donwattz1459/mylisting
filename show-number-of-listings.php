e518fc1fc097958d8731ae4705e7251934cb3084
donwattz1459 <mcguinn.donald@gmail.com>
2020-01-12 17:22:30 -0500

    Create show-number-of-listings.php

---
+++ show-number-of-listings.php
@@ -0,0 +1,5 @@
+// Display the total number of published listings using the shortcode [total_listings]
+function number_of_listings($atts) {
+ return wp_count_posts('job_listing')->publish;
+}
+add_shortcode('total_listings', 'number_of_listings');
