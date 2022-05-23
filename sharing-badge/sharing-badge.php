58e636eea241ec5bfb1cd58c67845ab5af0b0d58
donwattz1459 <mcguinn.donald@gmail.com>
2022-05-23 11:54:36 -0400

    Update sharing-badge.php

--- sharing-badge/sharing-badge.php
+++ sharing-badge/sharing-badge.php
@@ -4,34 +4,49 @@
 //ADD CUSTOM CLASS TO SHORTCODE ELEMENT IN SINGLE LISTING.
 
 <?php
-	//ADD CLASS TO THE BODY OF THE SINGLE LISTING
-	//===========================================
-	add_filter( 'body_class', function( $classes ) {
-		if ( is_singular( 'job_listing' ) ) {
-			global $post;
-			if ( absint( get_current_user_id() ) === absint( $post->post_author ) ) {
-				$classes[] = 'is-listing-author';
-			}
-		}
-	 
-		return $classes;
-	} );
-	
-	//GET CURRENT PAGE URL
-	//USE SHORTCODE [get_url] TO SHOW URL ON FRONT END
-	//===========================================
-	add_shortcode ('geturl', 'get_current_page_url');
-	function get_current_page_url() {
-		$pageURL = 'http';
-		if( isset($_SERVER["HTTPS"]) ) {
-			if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
-		}
-		$pageURL .= "://";
-		if ($_SERVER["SERVER_PORT"] != "80") {
-			$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
-		} else {
-			$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
-		}
-		return $pageURL;
+	add_shortcode ('get_dark_badge', 'getdarkbadge');
+function getdarkbadge() {
+	$pageURL = 'http';
+	if( isset($_SERVER["HTTPS"]) ) {
+		if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
 	}
+	$pageURL .= "://";
+	if ($_SERVER["SERVER_PORT"] != "80") {
+		$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+	} else {
+		$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+	}
+	return '<a href="' . $pageURL . '"><img style="width: 175px !important;" src="https://gotriber.com/wp-content/uploads/2019/10/Find-Us-On-Triber-Badge.png" /></a>';
+}
+
+add_shortcode ('get_light_blue_badge', 'getlightbluebadge');
+function getlightbluebadge() {
+	$pageURL = 'http';
+	if( isset($_SERVER["HTTPS"]) ) {
+		if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+	}
+	$pageURL .= "://";
+	if ($_SERVER["SERVER_PORT"] != "80") {
+		$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+	} else {
+		$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+	}
+	return '<a href="' . $pageURL . '"><img style="width: 175px !important;" src="https://gotriber.com/wp-content/uploads/2019/10/Find-Us-On-Triber-Badge-Minimal.png" /></a>';
+}
+
+add_shortcode ('get_black_badge', 'getblackbadge');
+function getblackbadge() {
+	$pageURL = 'http';
+	if( isset($_SERVER["HTTPS"]) ) {
+		if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+	}
+	$pageURL .= "://";
+	if ($_SERVER["SERVER_PORT"] != "80") {
+		$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+	} else {
+		$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+	}
+	return '<a href="' . $pageURL . '"><img style="width: 175px !important;" src="https://gotriber.com/wp-content/uploads/2019/10/Find-Us-On-Triber-Badge-B&amp;W.png" /></a>';
+}
+
 ?>
